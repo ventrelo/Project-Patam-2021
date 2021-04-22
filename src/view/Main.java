00efbdd1ca807c73715993126066bf5137f24d2e
David <davidr123451@gmail.com>
2021-04-22 13:18:28 +0300

    Finished setting up the file explorer. Rearranged the project so the it will comply with mvvm Arch

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,12 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.XMLParserModel;
+import vm.mainVM;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+
+        mainController mainC; // Main Controller
+        model.XMLParserModel xmlParserModel = new XMLParserModel(); //xmlParserModel
+        vm.mainVM mainVM = new mainVM(xmlParserModel); //main View Model
+        xmlParserModel.addObserver(mainVM);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainView.fxml"));
+        Parent root = loader.load();
+        mainC = loader.getController();
+        mainC.setMainVM(mainVM);
+        mainVM.addObserver(mainC);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
--- src/view/mainController.java
+++ src/view/mainController.java
@@ -1,6 +1,8 @@
 package view;
 
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -8,23 +10,24 @@ import javafx.scene.Node;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import vm.mainVM;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 
-public class mainController {
-
-     public vm.mainVM mainVM;
-     private HashMap xmlSettings = null;
-
+public class mainController implements Observer {
 
 
+    public vm.mainVM mainVM;
+    public void setMainVM(mainVM vm)
+    {
+        this.mainVM = vm;
 
-
-
-
+    }
 
     public void openFileExplorer(ActionEvent event) {
 
@@ -34,12 +37,17 @@ public class mainController {
 
         if (file != null) {
 
-            xmlSettings = model.XMLParser.decodeXML(file.getAbsolutePath());
-                if(xmlSettings != null)
+            mainVM.parseXML(file.getAbsolutePath());
+                if(mainVM.xmlSettings != null)
                 {
-                    System.out.println(xmlSettings);
+                    System.out.println(mainVM.xmlSettings);
                 }
                 else System.out.println("Fail");
         }
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
